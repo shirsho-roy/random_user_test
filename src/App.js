e49f6ceaa5c5e5d6fe087b63d09076f9e44be16c
shirsho-roy <shirsho.tffs@gmail.com>
2023-05-25 12:02:51 +0530

    Comments

--- src/App.js
+++ src/App.js
@@ -4,8 +4,9 @@ import './App.css';
 
 function App() {
   const url='https://randomuser.me/api/';
+  //State hook for array
   const [query,setQuery]=useState([]);
-  
+  //Fetch user info
       const fetchData = async () => {
         const res = await fetch(url);
         const {results} = await res.json();
@@ -16,9 +17,10 @@ function App() {
     <div className="App">
        <h1> Fetch data from an api in react </h1>  
        <button onClick={fetchData}>Change</button>
-       {
+       {        //Map the query
                 query.map((item) => ( 
                 <div >
+                  <h1>data display</h1>
                     <h2>Full Name: { item.name.first } {item.name.last}</h2> 
                     <h3>User Email: { item.email }</h3>
                     <h3>Age: {item.registered.age}</h3>
